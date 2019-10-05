a9bfcdd70e91e2428afdb91d482aefb0b0678055
erpisshit <56169032+erpisshit@users.noreply.github.com>
2019-10-05 19:53:03 +0000

    Volume
    Adding Volume to Main

--- Sphere.java
+++ Sphere.java
@@ -1,1 +1,16 @@
-
+class Sphere
+{
+  long r;
+  public Sphere(long r)
+  {
+   this.r = r; 
+  }
+  public long Area()
+  {
+    return 4*Math.pi*r*r;
+  }
+  public long Volume()
+  {
+    return (4/3)*Math.pi*r*r*r;
+  }
+}
