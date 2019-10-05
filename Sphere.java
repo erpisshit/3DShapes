ff41e93a3541700fd2ceee8c61516684dbaaa2d9
erpisshit <56169032+erpisshit@users.noreply.github.com>
2019-10-05 19:54:36 +0000

    Diameter
    Adding Diameter to Main

--- Sphere.java
+++ Sphere.java
@@ -1,1 +1,21 @@
+class Sphere
+{
+  long r;
+  public Sphere(long r)
+  {
+   this.r = r; 
+  }
+  public long Area()
+  {
+    return 4*Math.pi*r*r;
+  }
+  public long Volume()
+  {
+    return (4/3)*Math.pi*r*r*r;
+  }
+  public long Diameter()
+  {
+    return 2*r;
+  }
+}
 
