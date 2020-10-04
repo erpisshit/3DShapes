4fe06f91ed152adf70026927a5201a073739a6f7
erpisshit <56169032+erpisshit@users.noreply.github.com>
2020-10-04 06:06:04 +0530

    Initialization of Cube.java

---
+++ Cube.java
@@ -0,0 +1,11 @@
+import java.utils.*;
+public class Cube
+{
+  public static void main(Strings[] args)
+  {
+    Scanner jk = new Scanner(System.in);
+    System.out.println("Enter Size of side of Cube");
+    int a = jk.nextInt();
+  }
+
+}
