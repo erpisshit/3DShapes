3400e33e42eed1de8d8bfec1bf88fd1206fe37cd
iamspotify <72426323+iamspotify@users.noreply.github.com>
2020-10-06 08:49:23 +0530

    Area of Surface

---
+++ Cuboid.java
@@ -0,0 +1,24 @@
+import java.util.*
+public class Cuboid
+{
+  long r;
+  long p;
+  long q;
+  public Cuboid(long r, long p, long q)
+  {
+   this.r = r; 
+   this.p = p; 
+   this.q = q; 
+  }
+  public long Area(int a, int b)
+  {
+    return a*b;
+  }
+  public static void main(Strings[] args)
+  {
+    Scanner jk = new Scanner(System.in);
+    long r = jk.nextInt();
+    long p = jk.nextInt();
+    long q = jk.nextInt();
+  }
+}
