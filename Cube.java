343433afb5e74519c3f0f98b15de61ff115ee85a
iamspotify <72426323+iamspotify@users.noreply.github.com>
2021-10-30 03:54:39 +0530

    Coloured Cube and Total Area

--- Cube.java
+++ Cube.java
@@ -12,7 +12,31 @@ public class Cube
     this.org = new int[2];
     this.sides = new int[6];
   }
-  
+  public void tArea()
+  {
+    long sd = this.a * this.a;
+    long sd2 = 6*this.a;
+    System.out.println("Total Surface area of cube is "+sd2);
+  }
+  public void colorCheck()
+  {
+    if (this.sides[0]==1)
+    {
+      System.out.println("Red");
+    }
+    else if (this.sides[1]==2)
+    {
+      System.out.println("Blue");
+    }
+    else if (this.sides[1]==3)
+    {
+      System.out.println("Green");
+    }
+    else
+    {
+      System.out.println("Rainbow");
+    }
+  }
   public static void main(Strings[] args)
   {
     Scanner jk = new Scanner(System.in);
