d89ace0855de0a3b56246becaa7a1777d73e4e17
iamspotify <72426323+iamspotify@users.noreply.github.com>
2021-10-29 22:08:51 +0000

    Initialization of Class File

---
+++ Cyclinder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Cylinder
+{
+ 
+ public static void main(Strings[] args)
+ {
+   Scanner jk = new Scanner(System.in);
+   System.out.print("Enter Radius of Cylinder");
+   int r = jk.nextInt();
+   System.out.print("Enter Length of Cylinder");
+   int l = jk.nextInt();
+ }
+}
